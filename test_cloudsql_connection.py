00eba4f2db3d31c7b7c27a3b80ce0fbc8652ed24
aitechroberts <jrobert2@andrew.cmu.edu>
2024-09-22 13:53:58 -0400

    completed Dataproc cluster connection to CloudSQL, now making the initialization repeatable

--- test_cloudsql_connection.py
+++ test_cloudsql_connection.py
@@ -6,14 +6,14 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 # JDBC connection properties
-jdbc_url = "jdbc:postgresql://google/postgres?cloudSqlInstance=systool-436201:us-central1:postgres&socketFactory=com.google.cloud.sql.postgres.SocketFactory"  # Use the CloudSQL instance name, not IP
+jdbc_url = "jdbc:postgresql://google/postgres?cloudSqlInstance=systool-436201:us-central1:postgres&socketFactory=com.google.cloud.sql.postgres.SocketFactory"
 connection_properties = {
     "user": "postgres",
     "password": "pg_password",
     "driver": "org.postgresql.Driver"
 }
 
-# Test: Read from a table (make sure a table exists)
+# Test: Read from a table (make sure a table exists in your DB)
 try:
     df = spark.read.jdbc(url=jdbc_url, table="your_table_name", properties=connection_properties)
     df.show()  # Show first few rows from the table
